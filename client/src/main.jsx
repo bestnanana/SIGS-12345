f1c524f9a6067d8645451bdeb27e3a280fec151c
bestnanana <2584458943@qq.com>
2026-05-26 23:08:48 +0800

    backup: campus-service-platform snapshot %date%

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import App from "./App.jsx";
+import { LanguageProvider } from "./i18n.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LanguageProvider>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </LanguageProvider>
+  </React.StrictMode>
+);
